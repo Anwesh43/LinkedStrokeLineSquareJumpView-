039afda3c52f36a6e02ec6f62c7373699690b977
Anwesh43 <anweshthecool0@gmail.com>
2020-07-05 02:52:59 +0530

    adding Constants and libraries

--- strokelinesquarejumpview/src/main/java/com/anwesh/uiprojects/strokelinesquarejumpview/StrokeLineSquareJumpView.kt
+++ strokelinesquarejumpview/src/main/java/com/anwesh/uiprojects/strokelinesquarejumpview/StrokeLineSquareJumpView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.strokelinesquarejumpview
 /**
  * Created by anweshmishra on 05/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf("#3F51B5", "#F44336", "#009688", "#2196F3", "#4CAF50")
+val lines : Int = 4
+val scGap : Float = 0.02f / (lines + 1)
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
